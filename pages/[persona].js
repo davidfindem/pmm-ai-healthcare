12d755ae4b9966cd6dda9f225bb5bf23e97c66fd
davidfindem <david@findem.ai>
2025-07-08 15:07:48 -0700

    Update [persona].js

--- pages/[persona].js
+++ pages/[persona].js
@@ -1,18 +1,14 @@
-// pages/[persona].js
-import { useRouter } from 'next/router'
-import LandingPageTemplate from '../components/LandingPageTemplate'
-import allPersonas from '../data/personas'
+import { useRouter } from 'next/router';
+import LandingPageTemplate from '../components/LandingPageTemplate';
+import personas from '../data/personas';
 
 export default function PersonaPage() {
-  const router = useRouter()
-  const { persona } = router.query
+  const router = useRouter();
+  const { persona } = router.query;
 
-import allPersonas from '../data/personas'
+  const personaData = persona ? personas[persona] : null;
 
-const personaData = allPersonas[persona]
+  if (!personaData) return <div>Loading...</div>;
 
-
-  if (!personaData) return <div>Loading...</div>
-
-  return <LandingPageTemplate persona={personaData} />
+  return <LandingPageTemplate persona={personaData} />;
 }
